487c1936354775defc12c94b6ad090be103943db
Alex Kozminykh <sa.kozminykh@gmail.com>
2023-05-09 18:01:13 +0300

    GetMapping change

--- src/main/kotlin/ru/onlyfriends/api/controller/BloggerPostsController.kt
+++ src/main/kotlin/ru/onlyfriends/api/controller/BloggerPostsController.kt
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.*
 import ru.onlyfriends.api.service.getBloggerPostsService.ResponseService.BloggerPostsResponseService
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/posts/users")
 class BloggerPostsController(
     val service: BloggerPostsResponseService
 ) {
 //    @GetMapping("/{email}/posts")
-    @GetMapping("/posts/users/{email}")
+    @GetMapping("/{email}")
     fun getPosts(
         @PathVariable email: String,
         @RequestParam("since") since: String,
