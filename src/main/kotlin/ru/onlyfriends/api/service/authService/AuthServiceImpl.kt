d37adaa1ccf94c09830feca32bf2ddcd9731b675
Alex Kozminykh <sa.kozminykh@gmail.com>
2023-05-13 17:24:08 +0300

    Nicknames added

--- src/main/kotlin/ru/onlyfriends/api/service/authService/AuthServiceImpl.kt
+++ src/main/kotlin/ru/onlyfriends/api/service/authService/AuthServiceImpl.kt
@@ -4,6 +4,7 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import ru.onlyfriends.api.model.dto.TokenResponse
 import ru.onlyfriends.api.model.dto.exception.EmailAlreadyTakenException
+import ru.onlyfriends.api.model.dto.exception.NicknameAlreadyTakenException
 import ru.onlyfriends.api.model.dto.request.SignUpRequest
 import ru.onlyfriends.api.model.repository.UserRepository
 import ru.onlyfriends.api.utils.JwtTokenUtil
@@ -14,8 +15,12 @@ class AuthServiceImpl(
     private val jwtTokenUtil: JwtTokenUtil,
     private val passwordEncoder: PasswordEncoder
 ) : AuthService {
-    override fun signUp(signUpRequest: SignUpRequest): TokenResponse {
-        if (userRepository.existsByEmail(signUpRequest.email)) throw EmailAlreadyTakenException()
+    override fun signUp(signUpRequest:
+                        SignUpRequest): TokenResponse {
+        if (userRepository.existsByEmail(signUpRequest.email))
+            throw EmailAlreadyTakenException()
+        else if(userRepository.existsByNickname(signUpRequest.nickname))
+            throw NicknameAlreadyTakenException()
         signUpRequest.password = passwordEncoder.encode(signUpRequest.password)
         val user = userRepository.save(signUpRequest.asModel())
         val token: String = jwtTokenUtil.generateToken(user.email)
