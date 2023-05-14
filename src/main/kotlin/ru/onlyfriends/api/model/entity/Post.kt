1c1183fc8e20f2ce485ceb12bff7d96c0e9085cb
Alex Kozminykh <sa.kozminykh@gmail.com>
2023-05-14 23:10:31 +0300

    Posts are returned with author full info

--- src/main/kotlin/ru/onlyfriends/api/model/entity/Post.kt
+++ src/main/kotlin/ru/onlyfriends/api/model/entity/Post.kt
@@ -1,8 +1,5 @@
 package ru.onlyfriends.api.model.entity
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo
-import com.fasterxml.jackson.annotation.JsonIdentityReference
-import com.fasterxml.jackson.annotation.ObjectIdGenerators
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
@@ -14,8 +11,6 @@ import ru.onlyfriends.api.model.entity.like.Likable
 
 @Entity
 class Post(
-    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator::class, property = "id")
-    @JsonIdentityReference(alwaysAsId = true)
     @ManyToOne(fetch = FetchType.LAZY)
     val author: User,
     @Column(length = 255)
